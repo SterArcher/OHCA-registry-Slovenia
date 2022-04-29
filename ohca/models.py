223c8cb8213ca2f01d7d7aa104f3f8c7259cd97a
Gašper Šircelj <sircelj98@gmail.com>
2022-04-29 21:33:15 +0200

    EuReCa variables update: max IntegerField values reduced by 1, timeTCPR added

--- ohca/models.py
+++ ohca/models.py
@@ -147,7 +147,7 @@ class CaseReport(models.Model):
     hospitalArrival6Time = models.IntegerField(null = True, blank = True)
     hospArri = models.SmallIntegerField(null = True, blank = True, validators=[MinValueValidator(-1), MaxValueValidator(4)])
     dischDay = models.SmallIntegerField(null = True, blank = True)
-    timeTCPR = NULL
+    timeTCPR = models.IntegerField(null = True, blank = True)
 
     def update(self, *args, **kwargs):
         for name,values in kwargs.items():
