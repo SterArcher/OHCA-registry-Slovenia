f8bfbdc8733eb2c2aaded0dc92c8e2b5b36c9a92
evgenija <evgenijjaa@gmail.com>
2022-07-24 20:10:56 +0200

    add DateTimeSelector widget

--- ohca/fields.py
+++ ohca/fields.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.validators import RegexValidator
+from django.forms import TimeInput
+
 
 class InterventionWidget(forms.widgets.MultiWidget):
     def __init__(self, attrs=None):
@@ -22,3 +24,40 @@ class InterventionField(forms.MultiValueField):
 
     def compress(self, values):                                                
         return ''.join(values)
+
+# https://nancylin.xyz/how-to-implement-date-time-picker-in-django-without-javascript/
+
+from django import forms
+from django.forms import MultiWidget, TimeInput
+
+
+class DatePickerInput(forms.DateInput):
+    input_type = 'date'
+
+TimeWidgetSeconds = TimeInput(format='%H:%M:%S', attrs={'type': 'time', 'step' : 1})
+
+class DateTimePickerInput(forms.DateTimeInput):
+    input_type = 'datetime'
+
+class TimePickerInputSeconds(forms.TimeInput):
+    input_type = "time"
+
+from datetime import datetime
+
+class DateTimeSelector(forms.widgets.MultiWidget):
+    def __init__(self, attrs=None):
+        widgets = [
+            DatePickerInput(attrs=attrs),
+            TimePickerInputSeconds(format='%H:%M:%S', attrs={'type': 'time', 'step' : 1})
+        ]
+        super(DateTimeSelector, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if isinstance(value, datetime):
+            return [value.day, value.month, value.year, value.hour, value.minute, value.second]
+            # return [datetime(value.year, value.month, value.day, value.hour, value.minute, value.second)]
+        return [None, None, None, None, None, None]
+
+    def value_from_datadict(self, data, files, name):
+        date, time = super(DateTimeSelector, self).value_from_datadict(data, files, name)
+        return date + ' ' + time
