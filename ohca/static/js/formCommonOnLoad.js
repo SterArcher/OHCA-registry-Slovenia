0118939d6c4a90b9e029fe9912b2fcd7282ba949
Gašper Tomšič <gapy.tomsic@icloud.com>
2022-07-26 23:20:56 +0200

    DateFields now autocomplete on date of OHCA or time of call field change

---
+++ ohca/static/js/formCommonOnLoad.js
@@ -0,0 +1,16 @@
+const datepickers = document.getElementsByClassName("datepicker")
+
+const callTimeStampField = document.getElementById("id_callTimestamp_0")
+const dateOfCAField = document.getElementById("id_Date")
+callTimeStampField.addEventListener('onchange', updateDateFields())
+dateOfCAField.addEventListener('onchange', updateDateFields())
+
+function updateDateFields() {
+    const excludedIds = [callTimeStampField.id, dateOfCAField.id, "id_Date_birth"]
+    var date = callTimeStampField.
+    datepickers.array.forEach(datepicker => {
+        if (!(excludedIds.includes(datepicker.id))) {
+            datepicker.value = date
+        }
+    });
+}
