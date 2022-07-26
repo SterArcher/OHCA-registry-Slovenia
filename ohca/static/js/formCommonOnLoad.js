f729a5aaf4eb86e68af2ed0c9ebedb161578bc9b
Gašper Tomšič <gapy.tomsic@icloud.com>
2022-07-26 23:32:19 +0200

    Added support for more browsers

--- ohca/static/js/formCommonOnLoad.js
+++ ohca/static/js/formCommonOnLoad.js
@@ -2,12 +2,11 @@ const datepickers = document.getElementsByClassName("datepickerinput")
 
 const callTimeStampField = document.getElementById("id_callTimestamp_0")
 const dateOfCAField = document.getElementById("id_Date")
-callTimeStampField.addEventListener('onchange', updateDateFields())
-dateOfCAField.addEventListener('onchange', updateDateFields())
+callTimeStampField.addEventListener('onchange', updateDateFields(callTimeStampField.value))
+dateOfCAField.addEventListener('onchange', updateDateFields(dateOfCAField.value))
 
-function updateDateFields() {
+function updateDateFields(date) {
     const excludedIds = [callTimeStampField.id, dateOfCAField.id, "id_Date_birth"]
-    var date = callTimeStampField.value
     for (let i = 0; i < datepickers.length; i++) {
         if (!(excludedIds.includes(datepickers[i].id))) {
             datepickers[i].value = date
