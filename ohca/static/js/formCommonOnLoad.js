eb532c47aca55ea388acf1172538d693b40781dd
Gašper Tomšič <gapy.tomsic@icloud.com>
2022-07-30 22:39:45 +0200

    Fixed bug
    Some datepickers would not update on forms without a callTimeStampField

--- ohca/static/js/formCommonOnLoad.js
+++ ohca/static/js/formCommonOnLoad.js
@@ -2,8 +2,12 @@ const datepickers = document.getElementsByClassName("datepickerinput")
 
 const callTimeStampField = document.getElementById("id_callTimestamp_0")
 const dateOfCAField = document.getElementById("id_Date")
-callTimeStampField.addEventListener('change', function() { updateDateFields(callTimeStampField) })
-dateOfCAField.addEventListener('change', function() { updateDateFields(dateOfCAField) })
+if (callTimeStampField) {
+    callTimeStampField.addEventListener('change', function() { updateDateFields(callTimeStampField) })
+}
+if (dateOfCAField) {
+    dateOfCAField.addEventListener('change', function() { updateDateFields(dateOfCAField) })
+}
 
 function updateDateFields(caller) {
     let date = caller.value
