d3910f35703147b90a9bcb3ea08cc0bc868600bc
Gašper Tomšič <gapy.tomsic@icloud.com>
2022-08-04 12:28:21 +0200

    Fixed API

--- ohca/functions.py
+++ ohca/functions.py
@@ -3,26 +3,26 @@ from ohca.models import *
 def to_CaseReport(json, caseKey):
     entry = None
     if caseKey == 'dispatchID':
-        entry = CaseReport.objects.filter(dispatchID__exact=json['dispatchID'])[0]
+        entry = CaseReport.objects.update_or_create(dispatchID=json['dispatchID'], defaults=json)
     elif caseKey == 'caseID':
-        entry = CaseReport.objects.filter(caseID__exact=json['caseID'])[0]
-    return entry.update(**json)
+        entry = CaseReport.objects.update_or_create(caseID=json['caseID'], defaults=json)
+    return entry[0]
 
 def to_Locale(json):
     entry = None
     if id == 'localID':
-        entry = CaseReport.objects.filter(localID__exact=json['localID'])[0]
+        entry = CaseReport.objects.update_or_create(localID=json['localID'], defaults=json)
     elif id == 'friendlyName':
-        entry = CaseReport.objects.filter(friendly__exact=json['friendlyName'])[0]
-    return entry.update(**json)
+        entry = CaseReport.objects.update_or_create(friendly=json['friendlyName'], defaults=json)
+    return entry[0]
 
 def to_System(json, id):
     entry = None
     if id == 'systemID':
-        entry = CaseReport.objects.filter(systemID__exact=json['systemID'])[0]
+        entry = CaseReport.objects.update_or_create(systemID=json['systemID'], defaults=json)
     elif id == 'friendlyName':
-        entry = CaseReport.objects.filter(friendly__exact=json['friendlyName'])[0]
-    return entry.update(**json)
+        entry = CaseReport.objects.update_or_create(friendly=json['friendlyName'], defaults=json)
+    return entry[0]
 
 def validate_post(request):
     return request.method == 'POST'
