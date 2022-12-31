b4bcf45655454d69b196c2eb9807531a3cd1ded0
Miguel27 <97241847+VladChocola@users.noreply.github.com>
2022-12-31 11:41:59 +0100

    confidence_intervals
    Confidence_intervals.rs file now has z values (for 85% 90% and 95%) which is necessary for confidence intervals bigger than 30 elements

--- ohca-utstein/src/confidence_Intervals.rs
+++ ohca-utstein/src/confidence_Intervals.rs
@@ -1,16 +1,28 @@
 use std::io;
 use std::num::Float;
 
-fn main() {
+
+//1. Find the sample mean
+//2. Calculate the standard deviation
+//3. Find the standard error
+//4. Find the margin of error
+//5. Use these results in the formula
+
+
 let sample_size
 let standard_deviation
 let standard_error
 let margin_error
-let confidence
+let confidence // in decimals ( for 90% use 0.9 for example)
 let confidence_interval
 let mean
 let values = vec![0; sample_size];
 let sum
+let z_value
+
+fn main() {
+
+//WIP
 
 }
 
@@ -51,12 +63,33 @@ fn Calculate_standard_error(sample_size: i32, standard_deviation: f32)   -> f32
 }
 
 fn Calculate_margin_error(sample_size) -> f32 {
-
     1 / sqrt(sample_size)
-
 }
 
-fn Calculate_confidence_interval(mean: f32, confidence: i32, standard_deviation: f32, sample_size:i32) ->f32   
+fn Calculate_confidence_interval(mean: f32, confidence: i32, standard_deviation: f32, sample_size:i32, z_value:f32) ->f32   
 {
+    if sample_size < 30{
   confidence_interval = mean + confidence (standard_deviation / sqrt(sample_size))
+} else {
+    confidence_interval = z * standard_deviation + (z * (standard_deviation / sqrt(sample_size)))
+
+
+}
+}
+
+fn Calculate_z_value(confidence: f32){
+
+    if  confidence = 0.85 {
+
+        return z = 1.44 
+    } else if confidence = 0.9 {
+
+        return z = 1.65
+    } else if confidence = 0.95{
+
+        return z = 1.96
+    } else {
+        print!("This is a WIP, for now we only have z values for the following confidence intervals -> 85% 90% 95%");
+    }
+
 }
